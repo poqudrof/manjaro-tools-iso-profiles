581b58c450d21c19e8e5dca48bdbf8e332d64dc0
udeved <artoo@manjaro.org>
2016-08-17 21:56:09 +0200

    lxqt-minimal: upd

--- mk-yaml.sh
+++ mk-yaml.sh
@@ -6,8 +6,8 @@ pkgs=$(pacman -Sgq $1)
 out=$1.yaml
 
 echo "- name: '$1'" > $out
-	echo "  description: '$1 Desktop'" >> $out
-	echo "  packages:" >> $out
+echo "  description: '$1 Desktop'" >> $out
+echo "  packages:" >> $out
 for p in ${pkgs[@]};do
 	echo "       - $p" >> $out
 done
